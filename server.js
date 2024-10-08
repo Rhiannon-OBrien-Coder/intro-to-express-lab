86f7b697052030d720e32b46c54335b659998849
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-10-07 21:02:38 -0500

    fourth prompt completed. cannot handle multiple queries.

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.listen(PORT, () => {
     console.log(`Express server is listening on port ${PORT}`)
 })
 
+//prompt 1
+
 app.get('/greetings/:name', (req, res)=>{
     res.send({
         msg: `Hello there, ${req.params.name}!`
@@ -17,6 +19,8 @@ function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
+//prompt 2
+
 app.get('/roll/:num', (req, res)=>{
     let a = parseInt(req.params.num)
     if (Number.isInteger(a)) {
@@ -26,6 +30,8 @@ app.get('/roll/:num', (req, res)=>{
         res.send(`Error: You must specify a number.`)
     }
 })
+
+//prompt 3
 
 const collectibles = [
     { name: 'shiny ball', price: 5.95 },
@@ -41,6 +47,41 @@ app.get('/collectibles/:index', (req, res)=>{
         res.send(`This item is not yet in stock. Check back soon!`)
     }
 })
+
+//prompt 4
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+app.get('/shoes', (req, res)=>{
+    let a = parseInt(req.query.minprice)
+    let z = parseInt(req.query.maxprice)
+    let c = req.query.type
+    console.log(c)
+    if(!Number.isNaN(a)){
+        console.log("filter on min")
+        let minPriceOutput = shoes.filter((shoe) => shoe.price >= a)
+        res.send(minPriceOutput)
+    } else if (!Number.isNaN(z)) {
+        let maxPriceOutput = shoes.filter((shoe) => shoe.price <= z)
+        res.send(maxPriceOutput)
+    } else if(typeof c !== 'undefined'){
+        let typeOutput = shoes.filter((shoe) => shoe.type === c)
+        res.send(typeOutput)
+    } else{
+        res.send(shoes)}
+})
+
+
+
+//catchall below
 
 app.get('/*', (req, res) => {
     res.send({
